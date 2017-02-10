d4cf57cf97d6a1cd878bc8232aa96f20ba5e3755
Almas khan <akkak@hh>
2017-02-10 11:46:59 -0500

    Complete Tictactoe game

---
+++ TicTacToe/TicTacToe.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxC3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxC2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxC1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxB1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxA3 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxA2 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxA1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxC3
+            // 
+            this.pictureBoxC3.Location = new System.Drawing.Point(217, 217);
+            this.pictureBoxC3.Name = "pictureBoxC3";
+            this.pictureBoxC3.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxC3.TabIndex = 9;
+            this.pictureBoxC3.TabStop = false;
+            this.pictureBoxC3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxC2
+            // 
+            this.pictureBoxC2.Location = new System.Drawing.Point(150, 217);
+            this.pictureBoxC2.Name = "pictureBoxC2";
+            this.pictureBoxC2.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxC2.TabIndex = 8;
+            this.pictureBoxC2.TabStop = false;
+            this.pictureBoxC2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxC1
+            // 
+            this.pictureBoxC1.Location = new System.Drawing.Point(84, 217);
+            this.pictureBoxC1.Name = "pictureBoxC1";
+            this.pictureBoxC1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxC1.TabIndex = 7;
+            this.pictureBoxC1.TabStop = false;
+            this.pictureBoxC1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxB3
+            // 
+            this.pictureBoxB3.Location = new System.Drawing.Point(217, 152);
+            this.pictureBoxB3.Name = "pictureBoxB3";
+            this.pictureBoxB3.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxB3.TabIndex = 6;
+            this.pictureBoxB3.TabStop = false;
+            this.pictureBoxB3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxB2
+            // 
+            this.pictureBoxB2.Location = new System.Drawing.Point(150, 152);
+            this.pictureBoxB2.Name = "pictureBoxB2";
+            this.pictureBoxB2.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxB2.TabIndex = 5;
+            this.pictureBoxB2.TabStop = false;
+            this.pictureBoxB2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxB1
+            // 
+            this.pictureBoxB1.Location = new System.Drawing.Point(84, 152);
+            this.pictureBoxB1.Name = "pictureBoxB1";
+            this.pictureBoxB1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxB1.TabIndex = 4;
+            this.pictureBoxB1.TabStop = false;
+            this.pictureBoxB1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxA3
+            // 
+            this.pictureBoxA3.Location = new System.Drawing.Point(217, 93);
+            this.pictureBoxA3.Name = "pictureBoxA3";
+            this.pictureBoxA3.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxA3.TabIndex = 3;
+            this.pictureBoxA3.TabStop = false;
+            this.pictureBoxA3.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxA2
+            // 
+            this.pictureBoxA2.Location = new System.Drawing.Point(150, 93);
+            this.pictureBoxA2.Name = "pictureBoxA2";
+            this.pictureBoxA2.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxA2.TabIndex = 2;
+            this.pictureBoxA2.TabStop = false;
+            this.pictureBoxA2.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBoxA1
+            // 
+            this.pictureBoxA1.Location = new System.Drawing.Point(84, 93);
+            this.pictureBoxA1.Name = "pictureBoxA1";
+            this.pictureBoxA1.Size = new System.Drawing.Size(51, 50);
+            this.pictureBoxA1.TabIndex = 1;
+            this.pictureBoxA1.TabStop = false;
+            this.pictureBoxA1.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TicTacToe.Properties.Resources.board;
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(348, 329);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 331);
+            this.Controls.Add(this.pictureBoxC3);
+            this.Controls.Add(this.pictureBoxC2);
+            this.Controls.Add(this.pictureBoxC1);
+            this.Controls.Add(this.pictureBoxB3);
+            this.Controls.Add(this.pictureBoxB2);
+            this.Controls.Add(this.pictureBoxB1);
+            this.Controls.Add(this.pictureBoxA3);
+            this.Controls.Add(this.pictureBoxA2);
+            this.Controls.Add(this.pictureBoxA1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe by Almas Khan";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxC1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxB1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBoxA1;
+        private System.Windows.Forms.PictureBox pictureBoxA2;
+        private System.Windows.Forms.PictureBox pictureBoxA3;
+        private System.Windows.Forms.PictureBox pictureBoxB1;
+        private System.Windows.Forms.PictureBox pictureBoxB2;
+        private System.Windows.Forms.PictureBox pictureBoxB3;
+        private System.Windows.Forms.PictureBox pictureBoxC1;
+        private System.Windows.Forms.PictureBox pictureBoxC2;
+        private System.Windows.Forms.PictureBox pictureBoxC3;
+    }
+}
+
---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,182 @@
+﻿/* TicTacToe.cs
+ * Assignment 2
+ * Revision History
+ *      Almas Khan: 2016.10.04: Created
+ *      Almas Khan: 2016.10.05: coded
+ *      Almas Khan: 2016.10.07: Debugged
+ *      
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// A class that shows form of tic tac toe game whose base class is Form class
+    /// </summary>
+    public partial class TicTacToe : Form
+    {
+        //Global variables declaration
+        bool turn = true;// true if X's turn, false if O's turn
+        int turnCount = 0;
+        const int noOfBoxes = 9; // constant declaration of no of boxes in tictactoe board
+        
+        //Declaration of images used in the program which are saved in the resources 
+        Image imageCross = global::TicTacToe.Properties.Resources.cross;
+        Image imageCircle = global::TicTacToe.Properties.Resources.circle;
+        Image imageBoard = global::TicTacToe.Properties.Resources.board;
+
+        /// <summary>
+        /// The default constructor of the class
+        /// </summary>
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method will check adjacent boxes if matched to declare winner, if any condition is true then
+        /// winnerFound variable is set to true and will passed to other method.
+        /// </summary>
+        private void checkAllPosibilities()
+        {
+            bool winnerFound = false;
+            
+            //horizontal Check
+            if ((pictureBoxA1.Image == pictureBoxA2.Image) && (pictureBoxA2.Image == pictureBoxA3.Image) && (!pictureBoxA1.Enabled))
+            {
+                winnerFound = true;
+            }
+            else if ((pictureBoxB1.Image == pictureBoxB2.Image) && (pictureBoxB2.Image == pictureBoxB3.Image) && (!pictureBoxB1.Enabled))
+            {
+                winnerFound = true;
+            }
+            else if ((pictureBoxC1.Image == pictureBoxC2.Image) && (pictureBoxC2.Image == pictureBoxC3.Image) && (!pictureBoxC1.Enabled))
+            {
+                winnerFound = true;
+            }
+
+            //Vertical Check
+            if ((pictureBoxA1.Image == pictureBoxB1.Image) && (pictureBoxB1.Image == pictureBoxC1.Image) && (!pictureBoxA1.Enabled))
+            {
+                winnerFound = true;
+            }
+            else if ((pictureBoxA2.Image == pictureBoxB2.Image) && (pictureBoxB2.Image == pictureBoxC2.Image) && (!pictureBoxA2.Enabled))
+            {
+                winnerFound = true;
+            }
+            else if ((pictureBoxA3.Image == pictureBoxB3.Image) && (pictureBoxB3.Image == pictureBoxC3.Image) && (!pictureBoxA3.Enabled))
+            {
+                winnerFound = true;
+            }
+
+            //Diagonal check
+            if ((pictureBoxA1.Image == pictureBoxB2.Image) && (pictureBoxB2.Image == pictureBoxC3.Image) && (!pictureBoxA1.Enabled))
+            {
+                winnerFound = true;
+            }
+            else if ((pictureBoxA3.Image == pictureBoxB2.Image) && (pictureBoxB2.Image == pictureBoxC1.Image) && (!pictureBoxC1.Enabled))
+            {
+                winnerFound = true;
+            }
+            //Method called to check winner and boolean value is passed in method
+            checkWinner(winnerFound);
+        }
+
+        /// <summary>
+        /// This method will check for winner by checking the boolean value passed by preceding
+        /// method and also check value of variable turn to declare whether X is winner or O is winner
+        /// </summary>
+        /// <param name="winnerFound">Boolean parameter, if any of the winning posibility met then 
+        /// this parameter will set to true and sent</param>
+        private void checkWinner(bool winnerFound)
+        {
+            string winner = "";
+            if (winnerFound)
+            {
+                if (turn)
+                {
+                    winner = "O";
+                }
+                else
+                {
+                    winner = "X";
+                }
+                MessageBox.Show(winner + " wins!");
+
+                //Method called to start game again, after winner is showed 
+                initializeNewGame();
+            }
+            else
+            {
+                if (turnCount == noOfBoxes)
+                {
+                    MessageBox.Show("It's a Draw!");
+
+                    //Method called to start game again, after game is draw
+                    initializeNewGame();
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method will set everything as at start of the game, so that new game can be 
+        /// played
+        /// </summary>
+        private void initializeNewGame()
+            {
+                turn = true;
+                turnCount = 0;
+                try
+                {
+                    foreach (Control control in Controls)
+                    {
+                        PictureBox pictureBox = (PictureBox)control;
+                        pictureBox.Enabled = true;
+                        pictureBox.Image = null;
+                        pictureBox1.Image = imageBoard;
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Error Occured : "+e.Message);
+                }
+            }
+
+        /// <summary>
+        /// This event handler will be called when any picturebox is clicked and will display image of 
+        /// circle or cross depending on value of turn variable. After displaying the image
+        /// picturbox will be disabled so that nobody can change image in between the game.
+        /// </summary>
+        /// <param name="sender">Contains a reference to the control/object that raised the event</param>
+        /// <param name="e"></param>
+        private void pictureBox_Click(object sender, EventArgs e)
+        {
+            PictureBox pictureBox = (PictureBox)sender;
+            if (turn)
+            {
+                pictureBox.Image = imageCross;
+            }
+            else
+            {
+                pictureBox.Image = imageCircle;
+            }
+            turn = !turn;
+            pictureBox.Enabled = false;
+            turnCount++;
+            checkAllPosibilities(); // Method Called 
+
+        }
+    }
+}
+
+       
+    
